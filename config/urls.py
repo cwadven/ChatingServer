981e0fd724cc63e9b268c5aef5779512e550f71d
이창우 <cwadven@naver.com>
2021-12-25 11:47:30 +0900

    chore: endpoint 변경
    - endpoint 변경

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from websocket import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat', include('websocket.urls')),
+    path('', views.index, name='index'),
+    path('chat/', include('websocket.urls')),
 ]
--- websocket/urls.py
+++ websocket/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = 'websocket'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/<str:room>', views.chat_room, name='room'),
+    path('<str:room>', views.chat_room, name='room'),
 ]
