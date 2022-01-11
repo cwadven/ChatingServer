faebb9bb509cf75222f6f0533dc1e0d721510ac7
Lee Chang Woo <cwadven@tendom.co.kr>
2022-01-11 13:15:42 +0900

    feat: 그룹 인원 세는 것 추가
    - 인원 수 세기 위함

--- websocket/models.py
+++ websocket/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class GroupCount(models.Model):
+    nickname = models.CharField(max_length=128)
+    groupname = models.CharField(max_length=128)
+    login_time = models.DateTimeField(db_column='createTime', auto_now_add=True, blank=True, null=True)
